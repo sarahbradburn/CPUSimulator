fdcf8c2e4209588669ce202b37ea98e2e6702a7f
SarahBradburn <sarahbradburn@u.boisestate.edu>
2015-08-13 10:33:09 -0600

    8/12

--- Simulator.c
+++ Simulator.c
@@ -19,28 +19,72 @@ int * instructionMemory;
 reg* registers; 
 int * memory; 
 
+void initializeRegisters() {
+	//malloc for array
+	registers = (int *)malloc(sizeof(reg)*32);
+	
+
+	registers[0] = "r0"; 
+	registers[1] = "at";
+	registers[2] = "v0";
+	registers[3] = "v1";
+	registers[4] = "a0";
+	registers[5] = "a1";
+	registers[6] = "a2";
+	registers[7] = "a3";
+
+	//temporary registers
+	int i = 8; 	
+	for(i; i < 18; i++) {
+		registers[i] = "t" + (i-8);
+	}
+	
+	//memory registers
+	int j = 18; 	
+	for(i; i < 27; i++) {
+		registers[j] = "t" + (j-18);
+	}
+
+	registers[27] = "k0"; 
+	registers[28] = "k1";
+	registers[29] = "gp";
+	registers[30] = "sp";
+	registers[31] = "ra";
+}
+
 
 char* registersToString() {
 	int i = 0; 	
-	for(i; i < sizeOf(reg); i++) {
-		
-	} 
+	for(i; i < sizeOf(registers); i++) {
+
+	}
 } 
 
-
 int main() {
-	//initialize registers
-
-	//initialize memory 
-	printf("hello world");
-
+	printf("Start Computer");
 	
+	//initialize instruction array (manually) 
+	
+
+	//initialize memory array 
+
+
+	//initialize registers
+	initializeRegisters(); 
+	
+	//start loop for clock, time driven simulation
 	int clock = 0; 
-		
-	//time driven
 	while(clock < 600) {
+
+		//switch statement for different instructions
+
+
+		//advance clock for instruction memory 
 		clock++;
 	}
+
+
+	//End simulation
 
 
 	return 0;
