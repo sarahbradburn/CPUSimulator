7552db77c956c8e2dd162a2fe01b56b9b198f859
SarahBradburn <sarahbradburn@u.boisestate.edu>
2015-08-17 17:53:17 -0600

    data structures set up, going to begin switch logic

--- Simulator.c
+++ Simulator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Reg.h"  //or should this be .h? 
 
 //Instruction set 
@@ -15,12 +16,30 @@ int memoryArray[100] = {0};
 int registerArray[32] = {0};	//should be array of structs
 */
 
+
+struct reg* createReg(char *type, int value) {
+	struct reg* newReg = (struct reg*) malloc (sizeof(struct reg));
+	
+#if 0
+	newReg->type = (char *) malloc(sizeof(char));
+	strcpy(
+#else
+	newReg->type = strdup(type);
+#endif
+	newReg->value = value;
+
+	return newReg;
+}
+
+
+
+
 int * instructionMemory; 
 struct reg* registers; 
 int * memory; 
 
 int programCounter;
-int sizeOfMemory; 
+int sizeOfMemory = 100; 
 
 void initializeRegisters() {
 	//malloc for array
@@ -38,8 +57,29 @@ void initializeRegisters() {
 	registers[7] = *createReg("a3", 0);
 
 
-	//temporary registers
+	registers[8] = *createReg("t0", 0);
+	registers[9] = *createReg("t1", 0);
+	registers[10] = *createReg("t2", 0);
+	registers[11] = *createReg("t3", 0);
+	registers[12] = *createReg("t4", 0);
+	registers[13] = *createReg("t5", 0);
+	registers[14] = *createReg("t6", 0);
+	registers[15] = *createReg("t7", 0);
+	registers[16] = *createReg("t8", 0);
+	registers[17] = *createReg("t9", 0);
 
+
+	registers[18] = *createReg("s0", 0);
+	registers[19] = *createReg("s1", 0);
+	registers[20] = *createReg("s2", 0);
+	registers[21] = *createReg("s3", 0);
+	registers[22] = *createReg("s4", 0);
+	registers[23] = *createReg("s5", 0);
+	registers[24] = *createReg("s6", 0);
+	registers[25] = *createReg("s7", 0);
+	registers[26] = *createReg("s8", 0);
+/*
+	//temporary registers
 	int i; 	
 	for(i = 8; i < 18; i++) {
 		
@@ -49,10 +89,14 @@ void initializeRegisters() {
 	//memory registers
 	int j; 	
 	for(j = 18; j < 27; j++) {
-		registers[i] = *createReg("s" + (i-18), 0);
+		char * letter = "s";
+		char * number = (char *) malloc(sizeof(char)*3); 
+		sprintf(number, "%d", j);
+
+		registers[i] = *createReg(strcat(letter, number), 0);
 	}
 
-
+*/
 	registers[27] = *createReg("k0", 0);
 	registers[28] = *createReg("k1", 0);
 	registers[29] = *createReg("gp", 0);
@@ -70,14 +114,47 @@ void printRegisters() {
 		printf("%s %d \n", registers[i].type, registers[i].value);
 
 	}
+	printf("\n");
 } 
+
+void printMemory() { 
+
+	printf("Memory \n");
+	int i; 	
+
+	//curently, all values are blank
+	for(i = 0; i < 100; i++) {
+		printf("%d %d \n", i, memory[i]);
+
+	}
+} 
+
+void initializeInstructionMemory() {
+	instructionMemory = (int*) calloc(50, sizeof(int));
+	instructionMemory[1] = 0x20080003;  //addi $t0, $zero, 3 
+	instructionMemory[1] = 0x20090004;  //addi $t1, $zero, 4 
+	instructionMemory[1] = 0x01095020;  //add $t2, $t0, $t1 
+	instructionMemory[1] = 0x0000000c;  //end
+}
+
+void printInstructionMemory() { 
+
+	printf("Memory \n");
+	int i; 	
+
+	//curently, all values are blank
+	for(i = 0; i < 100; i++) {
+		printf("%d %d \n", i, memory[i]);
+
+	}
+}
 
 int main() {
 	printf("Start Computer \n");
 	
 	//initialize instruction array (manually) 
 	//instructionMemory = (int*) calloc(sizeof(int)*50);
-	instructionMemory = (int*) calloc(50, sizeof(int));
+	initializeInstructionMemory();
 	
 	//initialize memory array 
 	//memory = (int*) calloc(sizeof(int)*sizeOfMemory);
@@ -86,12 +163,17 @@ int main() {
 	//initialize registers
 	initializeRegisters(); 
 	printRegisters();
-	
+	printMemory();	
+
 	//start loop for clock, time driven simulation
 	while(programCounter < 600) {
 
 		//switch statement for different instructions
+		switch(instruction) { 
+			
 
+
+		}
 
 		//advance clock for instruction memory 
 		programCounter++;
