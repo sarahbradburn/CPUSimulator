5ba0942433936f375a774bc24c617b89a040bbe3
sarahbradburn <sarahbradburn@u.boisestate.edu>
2015-08-18 13:47:22 -0600

    Update Reg.h

--- Reg.h
+++ Reg.h
@@ -14,28 +14,3 @@ struct reg* createReg(char *type, int value);
 //Should I have a free reg? 
 
 #endif
-
-
-
-/*
-
-#ifndef __NODE_H
-#define __NODE_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-struct node {
-	void *obj;
-	struct node *next;
-	struct node *prev;
-};
-
-struct node* createNode (void *obj);
-void freeNode(struct node *node, void (*freeObject)(void *));
-
-#endif 
-
-
-*/ 
-
